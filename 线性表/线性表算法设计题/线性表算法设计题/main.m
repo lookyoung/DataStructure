d0f3a3a57227ea6de13185c9533512ad9fdddbe4
刘洋 <liuyang003@wondertek.com.cn>
2020-05-03 18:18:10 +0800

    线性表算法设计题

--- 线性表/线性表算法设计题/线性表算法设计题/main.m
+++ 线性表/线性表算法设计题/线性表算法设计题/main.m
@@ -298,7 +298,7 @@ void DeleteMinMax(LinkList *L, int mink, int maxk){
     }
     
     //2.查找第一个值大于等于maxk的结点
-    while (p && p->data<=maxk) {
+    while (p && p->data <= maxk) {
         p = p->next;
     }
     
@@ -313,6 +313,170 @@ void DeleteMinMax(LinkList *L, int mink, int maxk){
     }
 }
 
+/*
+题目5:
+设将n(n>1)个整数存放到一维数组R中, 试设计一个在时间和空间两方面都尽可能高效的算法;将R中保存的序列循环左移p个位置(0<p<n)个位置, 即将R中的数据由(x0,x1,......,xn-1)变换为(xp,xp+1,...,xn-1,x0,x1,...,xp-1).
+
+例如: pre[10] = {0,1,2,3,4,5,6,7,8,9},
+     n = 10,p = 3;
+     pre[10] = {3,4,5,6,7,8,9,0,1,2}
+
+算法思路:
+1. 先将n个数据原地逆置 9,8,7,6,5,4,3,2,1,0;
+2. 将n个数据拆解成[9,8,7,6,5,4,3] [2,1,0]
+2. 将前n-p个数据和后p个数据分别原地逆置; [3,4,5,6,7,8,9] [0,1,2]
+
+复杂度分析:
+时间复杂度: O(n); 时间复杂度:O(1);
+*/
+
+//将数组R中的数据原地逆置
+void Reverse(int *pre,int left ,int right){
+    
+    //i等于左边界left,j等于右边界right;
+    int i = left;
+    int j = right;
+    int temp;
+    
+    //交换pre[i] 和 pre[j] 的值
+    while (i < j) {
+        temp = pre[i];
+        pre[i] = pre[j];
+        pre[j] = temp;
+        //i右移,j左移
+        i++;
+        j--;
+    }
+}
+
+//将长度为n的数组pre 中的数据循环左移p个位置
+void LeftShift(int *pre, int n, int p) {
+    
+    // 边界条件
+    if (p>0 && p<n) {
+        //1. 将数组中所有元素全部逆置
+        Reverse(pre, 0, n-1);
+        //2. 将前n-p个数据逆置
+        Reverse(pre, 0, n-p-1);
+        //3. 将后p个数据逆置
+        Reverse(pre, n-p, n-1);
+    }
+}
+
+/*
+  题目6:
+ 已知一个整数序列A = (a0,a1,a2,...an-1),其中(0<= ai <=n),(0<= i<=n). 若存在ap1= ap2 = ...= apm = x,且m>n/2(0<=pk<n,1<=k<=m),则称x 为 A的主元素. 例如,A = (0,5,5,3,5,7,5,5),则5是主元素; 若B = (0,5,5,3,5,1,5,7),则A 中没有主元素,假设A中的n个元素保存在一个一维数组中,请设计一个尽可能高效的算法,找出数组元素中的主元素,若存在主元素则输出该元素,否则输出-1.
+ 
+ 题目分析:
+ 主元素,是数组中的出现次数超过一半的元素; 当数组中存在主元素时,所有非主元素的个数和必少于一半. 如果让主元素和一个非主元素配对, 则最后多出来的元素(没有元素与之匹配就是主元素.
+ 
+ 算法思路:
+ 1. 选取候选主元素, 从前向后依次扫描数组中的每个整数, 假定第一个整数为主元素,将其保存在Key中,计数为1. 若遇到下一个整数仍然等于key,则计数加1. 否则计数减1. 当计数减到0时, 将遇到的下一个整数保存到key中, 计数重新记为1. 开始新一轮计数. 即可从当前位置开始重上述过程,直到将全部数组元素扫描一遍;
+ 2. 判断key中的元素是否是真正的主元素, 再次扫描数组, 统计key中元素出现的次数,若大于n/2,则为主元素,否则,序列中不存在主元素;
+ 
+ 算法分析:
+ 时间复杂度: O(n)
+ 空间复杂度: O(1)
+ */
+
+int MainElement(int *A, int n){
+    
+    // 求整数序列A的主元素
+    
+    // count 用来计数，类似于引用计数，而非 元素出现个数
+    int count = 1;
+    // 默认 数组首元素为 候选主元素key
+    int key = A[0];
+    
+    // 遍历一次，获取候选主元素 （不一定是出现次数最多的元素，不同排序，候选主元素可能不一样，当然 主元素跟排序无关）
+    for (int i = 1; i < n; i++) {
+        if (key == A[i]) {
+            // 若遍历的元素 == 候选主元素，则其计数+1
+            count++;
+        } else {
+            // 否则计数-1
+            if (count > 0) {
+                count--;
+            } else {
+                // 若计数减到0之后，不能再减，此时更换候选主元素，并重置计数为1
+                key = A[i];
+                count = 1;
+            }
+        }
+    }
+    
+    // 得到候选主元素后 获取其出现次数
+    if (count > 0) { //当计数count>0,才获取出现次数,否则没必要获取
+        for (int i = count = 0; i < n; i++) {
+            // 这里count 不再是计数，而是当作出现次数
+            if (key == A[i]) {
+                count++;
+            }
+        }
+    }
+    
+    // 候选主元素 和 其出现次数都有了，再判断是否主元素
+    if (count > n/2) {
+        return key;
+    } else {
+        return -1;
+    }
+    
+}
+
+
+/*
+ 题目7:
+ 用单链表保存m个整数, 结点的结构为(data,link),且|data|<=n(n为正整数). 现在要去设计一个时间复杂度尽可能高效的算法. 对于链表中的data 绝对值相等的结点, 仅保留第一次出现的结点,而删除其余绝对值相等的结点.例如,链表A = {21,-15,15,-7,15}, 删除后的链表A={21,-15,-7};
+ 
+ 题目分析:
+    要求设计一个时间复杂度尽量高效的算法,而已知|data|<=n, 所以可以考虑用空间换时间的方法. 申请一个空间大小为n+1(0号单元不使用)的辅助数组. 保存链表中已出现的数值,通过对链表进行一趟扫描来完成删除.
+ 算法思路:
+ 1. 申请大小为n+1的辅助数组t并赋值初值为0;
+ 2. 从首元结点开始遍历链表,依次检查t[|data|]的值, 若[|data|]为0,即结点首次出现,则保留该结点,并置t[|data|] = 1,若t[|data|]不为0,则将该结点从链表中删除.
+
+ 复杂度分析:
+ 时间复杂度: O(m),对长度为m的链表进行一趟遍历,则算法时间复杂度为O(m);
+ 空间复杂度: O(n)
+ */
+void DeleteEqualNode(LinkList *L,int n){
+    
+    // 开辟辅助 数组
+    int *arr = malloc(sizeof(int)*n);
+    // alloca
+    
+    // 数组元素初始值置空
+    for (int i = 0; i < n; i++) {
+        *(arr + i) = 0;
+    }
+    
+    // pre 用来保存待删除结点的前驱
+    LinkList pre = *L;
+    // 从链表 首元结点开始 遍历
+    LinkList temp = (*L)->next;
+    
+    // 遍历链表,直到temp = NULL;
+    while (temp != NULL) {
+        if (arr[abs(temp->data)] == 0) {
+            // 若数组中 下标值为对应链表结点绝对值 的数组元素 == 0, 则将其置为1
+            // 即 遍历到首次出现的结点,则将数组中对应位置置为1;
+            arr[abs(temp->data)] = 1;
+            // 同时记录下该结点
+            pre = temp;
+            // 继续向后遍历结点
+            temp = temp -> next;
+        } else {
+            // 遍历到 已经出现过的结点，则删除该结点
+            // 结点pre后继指向 待删除结点后继
+            pre->next = temp->next;
+            // 删除该结点
+            free(temp);
+            // 继续向后遍历结点
+            temp = temp->next;
+        }
+    }
+    
+}
 
 int main(int argc, const char * argv[]) {
     printf("线性表练习篇!\n");
@@ -363,18 +527,70 @@ int main(int argc, const char * argv[]) {
 //    printf("Lc:\n");
 //    ListTraverse(Lc);
     
-    /*---------作业3--------*/
-    printf("******题目3:********\n");
-    InitList(&L);
-    for(int j = 10;j>=0;j-=2)
-    {
-        iStatus = ListInsert(&L, 1, j);
-    }
-    printf("L逆转前:\n");
-    ListTraverse(L);
+//    /*---------作业3--------*/
+//    printf("******题目3:********\n");
+//    InitList(&L);
+//    for(int j = 10;j>=0;j-=2)
+//    {
+//        iStatus = ListInsert(&L, 1, j);
+//    }
+//    printf("L逆转前:\n");
+//    ListTraverse(L);
+//
+//    Inverse(&L);
+//    printf("L逆转后:\n");
+//    ListTraverse(L);
     
-    Inverse(&L);
-    printf("L逆转后:\n");
+//    /*---------作业4--------*/
+//    printf("******题目4:********\n");
+//    InitList(&L);
+//    for(int j = 10;j>=0;j-=2)
+//    {
+//        iStatus = ListInsert(&L, 1, j);
+//    }
+//    printf("L链表:\n");
+//    ListTraverse(L);
+//
+//    DeleteMinMax(&L, 2, 7);
+//    printf("删除链表mink与maxk之间结点的链表:\n");
+//    ListTraverse(L);
+    
+//    /*---------作业5--------*/
+//    printf("******题目5:********\n");
+//    int pre[10] = {0,1,2,3,4,5,6,7,8,9};
+//    LeftShift(pre, 10, 5);
+//    for (int i=0; i < 10; i++) {
+//        printf("%d ",pre[i]);
+//    }
+//    printf("\n");
+//
+
+//     /*---------作业6--------*/
+//    printf("******题目6:********\n");
+//    int  A[] = {0,5,5,3,5,7,5,5};
+//    int  B[] = {0,5,5,3,5,1,5,7};
+//    int  C[] = {0,1,1,1,2,2,2,2};
+//
+//    int value = MainElement(A, 8);
+//    printf("数组A 主元素为: %d\n",value);
+//    value = MainElement(B, 8);
+//    printf("数组B 主元素为(-1表示数组没有主元素): %d\n",value);
+//    value = MainElement(C, 8);
+//    printf("数组C 主元素为(-1表示数组没有主元素): %d\n",value);
+    
+
+    /*---------作业7--------*/
+    //21,-15,15,-7,15
+    printf("******题目7:********\n");
+    InitList(&L);
+    ListInsert(&L, 1, 21);
+    ListInsert(&L, 1, -15);
+    ListInsert(&L, 1, 15);
+    ListInsert(&L, 1, -7);
+    ListInsert(&L, 1, 15);
+    ListTraverse(L);
+
+    DeleteEqualNode(&L, 21);
     ListTraverse(L);
     
     return 0;
